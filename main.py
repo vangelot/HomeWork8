da9b47ba91da952f27d837213193ef404761f67d
vangelot <chuli@ukr.net>
2023-02-02 12:21:29 +0200

    1st commit after python

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def main_function()
+def main_function():
     print("1")
     print("2")
     print("3")
     print("new 4")
     print("new 5")
 
-def test_function()
+def test_function():
     print("Test for 1st commit")
     print("Test for 2nd commit")
